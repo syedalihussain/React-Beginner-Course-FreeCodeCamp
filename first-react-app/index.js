5a878dc45598c61c522487b15e4b37f2e133d4d7
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2022-08-06 16:28:10 +1000

    Challenge Imperative Way of writing dom. Solution

--- first-react-app/index.js
+++ first-react-app/index.js
@@ -1,2 +1,6 @@
-ReactDOM.render(<ul><li>Item 1</li><li>Item 2</li></ul>, document.getElementById('root'));
+// ReactDOM.render(<ul><li>Item 1</li><li>Item 2</li></ul>, document.getElementById('root'));
 
+const h1 = document.createElement('h1');
+h1.textContent = 'This is an imperative way of creating dom elements!';
+h1.className = 'header';
+document.getElementById('root').appendChild(h1);
