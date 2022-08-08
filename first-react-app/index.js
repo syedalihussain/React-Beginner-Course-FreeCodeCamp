2690e6bfea241d35bbefa3446a9b2b3799c6d160
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2022-08-08 23:53:10 +1000

    Move header to its own file. NOT WORKING CODE

--- first-react-app/index.js
+++ first-react-app/index.js
@@ -1,16 +1,28 @@
 // ReactDOM.render(<ul><li>Item 1</li><li>Item 2</li></ul>, document.getElementById('root'));
 
-function Page () {
-    return (
-        <div>
-            <h1>Reason's why I love react</h1>
-            <ol>
-                <li>It is more popular than Angular</li>
-                <li>It teaches so much about frontend</li>
-                <li>It is pays well</li>
-            </ol>
-        </div>
-    )
+import Header from "./Header"
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Reason's why I love react</h1>
+        <ol>
+          <li>It is more popular than Angular</li>
+          <li>It teaches so much about frontend</li>
+          <li>It is pays well</li>
+        </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <MainContent />
+      {/* <Footer /> */}
+    </div>
+  )
 }
 
 ReactDOM.render(<Page />, document.getElementById('root'))
