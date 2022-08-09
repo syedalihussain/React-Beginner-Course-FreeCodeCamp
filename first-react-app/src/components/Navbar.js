554923d410fcc2a4a86a408327c1289d43c7fb37
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2022-08-09 22:59:24 +1000

    Finish Navbar: Styling

--- first-react-app/src/components/Navbar.js
+++ first-react-app/src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
         className='nav--icon'
       />
       <h3 className="nav--logo_text">ReactFacts</h3>
-      <h3 className="nav--title_text">React Course - Project 1</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   )
 }
