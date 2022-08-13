b7cb2bc82a3b637f34ed6b710491081493d5a3c7
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2022-08-13 23:30:28 +1000

    Enhancements to Info section. Missing buttons

--- digital-business-card/src/components/Info.js
+++ digital-business-card/src/components/Info.js
@@ -2,7 +2,7 @@ import profilePicture from '../assets/profile_image.jpeg';
 
 export default function Info() {
   return (
-    <>
+    <div className="info">
       <img 
         src={profilePicture}
         alt='React Logo'
@@ -11,6 +11,6 @@ export default function Info() {
       <h2>Laura Smith</h2>
       <h4>Frontend Developer</h4>
       <p>Laurasmith Website</p>
-    </>
+    </ div>
   )
 }
