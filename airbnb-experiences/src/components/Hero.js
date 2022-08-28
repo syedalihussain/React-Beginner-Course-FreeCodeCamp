daaf2f8cffafb59fe878a5caa06c305a08b0d76a
Syed Ali Hussain <hussain.syedalishah@gmail.com>
2022-08-28 22:51:56 +1000

    Fix typo in className hero--text

--- airbnb-experiences/src/components/Hero.js
+++ airbnb-experiences/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero">
       <img src={PhotoGrid} className="hero--photo" />
       <h1 className="hero--header">Online Experirences</h1>
-      <p className="hero-text">Join unique interactive
+      <p className="hero--text">Join unique interactive
       activities led by one-of-a-kind hosts-all without leaving home</p>
     </section>
   )
